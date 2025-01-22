3f111de96ea53cf481ab806ef6cb4ec59b0e0e40
Natan <nydcastro22@src.pshs.edu.ph>
2025-01-22 20:28:57 +0800

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let cCircum = 2 * pi * r;
 let surfArea = (4 * pi) * (r ** 2);
 let vVolume = (4 / 3) * pi * (r ** 3);
 
-
+document.write('The radius is: ' + r + '.'+ '<br>');
 document.write('The circumference is: ' + cCircum + '.'+ '<br>');
 document.write('The surface area is: ' + surfArea + '.'+ '<br>');
 document.write('The volume is: ' + vVolume + '.'+ '<br>');
