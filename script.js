8b2d5537bbbd1d36cbad75001bccefc585fc5c3b
Natan <nydcastro22@src.pshs.edu.ph>
2025-01-22 13:48:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const pi = 3.14;
+let r = 2;
+
+
+let cCircum = 2 * pi * r;
+let surfArea = (4 * pi)*(r ** 2);
+let vVolume = (4 / 3) * pi * (r ** 3);
+
+
+document.write('The circumference is: ' + cCircum + '.'+ '<br>');
+document.write('The surface area is: ' + surfArea + '.'+ '<br>');
+document.write('The volume is: ' + vVolume + '.'+ '<br>');
