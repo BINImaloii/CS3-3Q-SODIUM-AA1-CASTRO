9580c37f10c960e09ad85db05530da2c9706c97b
Natan <nydcastro22@src.pshs.edu.ph>
2025-01-22 20:20:52 +0800

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const pi = 3.14;
-let r = 2;
+let r = 6378e3;
 
 
 let cCircum = 2 * pi * r;
-let surfArea = (4 * pi)*(r ** 2);
+let surfArea = (4 * pi) * (r ** 2);
 let vVolume = (4 / 3) * pi * (r ** 3);
 
 
